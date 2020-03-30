3e11f32178ea914ebf24b72ad657a0f0269c6656
Jiří Baran <jurka.baran@seznam.cz>
2020-03-30 17:55:15 +0200

    Přidejte soubory projektu.

---
+++ prepona_prav_troj/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prepona_prav_troj
+{
+    class Numbers
+    {
+        public double Soucet(double odvesna1, double odvesna2)
+        {
+            return Math.Sqrt(odvesna1 * odvesna1 + odvesna2 * odvesna2);
+        }
+    }
+}
---
+++ prepona_prav_troj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prepona_prav_troj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+
+            Console.WriteLine(numbers.Soucet(2, 3));
+            Console.ReadKey();
+            
+        }
+    }
+}
